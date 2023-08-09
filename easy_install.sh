13d4388ad02ab645863e11c2f761c6e1becc0c14
David Malone <dmmal@amazon.com>
2023-08-09 17:11:08 -0400

    seem to have all of the bugs worked out in easy_install after all the refactoring

--- easy_install.sh
+++ easy_install.sh
@@ -27,21 +27,21 @@ sudo mkdir -p $KVS_WEBRTC_HOME
 
 # generate run-kvs-webrtc.sh using outputs from previous setps
 echo "generating run-kvs-webrtc-client-master-sample.sh under $(pwd)/amazon-kinesis-video-streams-webrtc-sdk-c"
-cat > $(pwd)/amazon-kinesis-video-streams-webrtc-sdk-c/run-kvs-webrtc-client-master-sample.sh <<EOF
+cat > ./amazon-kinesis-video-streams-webrtc-sdk-c/run-kvs-webrtc-client-master-sample.sh <<EOF
 export AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION
 
 export AWS_IOT_CREDENTIALS_ENDPOINT=`cat $(pwd)/amazon-kinesis-video-streams-webrtc-sdk-c/iot/credential-provider-endpoint`
 export AWS_IOT_ROLE_ALIAS=`cat $(pwd)/amazon-kinesis-video-streams-webrtc-sdk-c/iot/role-alias`
 
-export IOT_CERT_PATH=`$KVS_WEBRTC_HOME`/iot/certs/device.cert.pem
-export IOT_PRIVATE_KEY_PATH=`$KVS_WEBRTC_HOME`/iot/certs/device.private.key
-export IOT_CA_CERT_PATH=`$KVS_WEBRTC_HOME`/iot/certs/root-CA.crt
-export AWS_KVS_CACERT_PATH=`$KVS_WEBRTC_HOME`/certs/cert.pem
+export IOT_CERT_PATH=$KVS_WEBRTC_HOME/iot/certs/device.cert.pem
+export IOT_PRIVATE_KEY_PATH=$KVS_WEBRTC_HOME/iot/certs/device.private.key
+export IOT_CA_CERT_PATH=$KVS_WEBRTC_HOME/iot/certs/root-CA.crt
+export AWS_KVS_CACERT_PATH=$KVS_WEBRTC_HOME/certs/cert.pem
 
 export DEBUG_LOG_SDP=TRUE
 export AWS_KVS_LOG_LEVEL=1
 
-`$KVS_WEBRTC_HOME`/build/samples/kvsWebrtcClientMasterGstSample `cat $(pwd)/amazon-kinesis-video-streams-webrtc-sdk-c/iot/thing-name`
+$KVS_WEBRTC_HOME/build/samples/kvsWebrtcClientMasterGstSample `cat $(pwd)/amazon-kinesis-video-streams-webrtc-sdk-c/iot/thing-name`
 EOF
 
 sudo chmod 755 ./amazon-kinesis-video-streams-webrtc-sdk-c/run-kvs-webrtc-client-master-sample.sh
