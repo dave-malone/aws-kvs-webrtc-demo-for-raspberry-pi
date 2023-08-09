69739c37203b20f2dd81d1b9c6d57e6020672b1c
David Malone <dmmal@amazon.com>
2023-08-09 15:53:53 -0400

    refactoring of statements that generate run-kvs-webrtc.sh

--- easy_install.sh
+++ easy_install.sh
@@ -17,6 +17,26 @@ sudo mkdir -p $KVS_WEBRTC_HOME
 
 ./iot/provision-thing.sh
 
+# generate run-kvs-webrtc.sh using outputs from previous setps
+cat > ./amazon-kinesis-video-streams-webrtc-sdk-c/run-kvs-webrtc-client-master-sample.sh <<EOF
+export AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION
+
+export AWS_IOT_CREDENTIALS_ENDPOINT=`cat ./amazon-kinesis-video-streams-webrtc-sdk-c/iot/credential-provider-endpoint`
+export AWS_IOT_ROLE_ALIAS=`cat ./amazon-kinesis-video-streams-webrtc-sdk-c/iot/role-alias`
+
+export IOT_CERT_PATH=`$KVS_WEBRTC_HOME/iot/certs/device.cert.pem`
+export IOT_PRIVATE_KEY_PATH=`$KVS_WEBRTC_HOME/iot/certs/device.private.key`
+export IOT_CA_CERT_PATH=`$KVS_WEBRTC_HOME/iot/certs/root-CA.crt`
+export AWS_KVS_CACERT_PATH=`$KVS_WEBRTC_HOME/certs/cert.pem`
+
+export DEBUG_LOG_SDP=TRUE
+export AWS_KVS_LOG_LEVEL=1
+
+`$KVS_WEBRTC_HOME/amazon-kinesis-video-streams-webrtc-sdk-c/build/samples/kvsWebrtcClientMasterGstSample` `cat ./amazon-kinesis-video-streams-webrtc-sdk-c/iot/thing-name`
+EOF
+
+sudo chmod 755 ./run-kvs-webrtc-client-master-sample.sh
+
 echo "moving amazon-kinesis-video-streams-webrtc-sdk-c to /opt/"
 sudo mv ./amazon-kinesis-video-streams-webrtc-sdk-c/* $KVS_WEBRTC_HOME
 
--- install-kvs-webrtc-service.sh
+++ install-kvs-webrtc-service.sh
@@ -1,26 +1,5 @@
 sudo cp ./kvs-webrtc.service /etc/systemd/system/kvs-webrtc.service
 
-# generate run-kvs-webrtc.sh using outputs from previous setps
-cat > ./run-kvs-webrtc-client-master-sample.sh <<EOF
-export AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION
-
-export AWS_IOT_CREDENTIALS_ENDPOINT=`cat $KVS_WEBRTC_HOME/iot/credential-provider-endpoint`
-export AWS_IOT_ROLE_ALIAS=`cat $KVS_WEBRTC_HOME/iot/role-alias`
-
-export IOT_CERT_PATH=`$KVS_WEBRTC_HOME/iot/certs/device.cert.pem`
-export IOT_PRIVATE_KEY_PATH=`$KVS_WEBRTC_HOME/iot/certs/device.private.key`
-export IOT_CA_CERT_PATH=`$KVS_WEBRTC_HOME/iot/certs/root-CA.crt`
-export AWS_KVS_CACERT_PATH=`$KVS_WEBRTC_HOME/certs/cert.pem`
-
-export DEBUG_LOG_SDP=TRUE
-export AWS_KVS_LOG_LEVEL=1
-
-`$KVS_WEBRTC_HOME/build/samples/kvsWebrtcClientMasterGstSample` `cat $KVS_WEBRTC_HOME/iot/thing-name`
-EOF
-
-sudo chmod 755 ./run-kvs-webrtc-client-master-sample.sh
-sudo mv ./run-kvs-webrtc-client-master-sample.sh $KVS_WEBRTC_HOME/
-
 sudo systemctl daemon-reload
 sudo systemctl enable kvs-webrtc.service
 sudo systemctl start kvs-webrtc.service
