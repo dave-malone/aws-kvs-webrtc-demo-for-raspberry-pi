fc448178dcc879e86792879fa9abc9b01c9235e7
David Malone <dmmal@amazon.com>
2023-08-09 15:07:00 -0400

    changing cmake commands to build using mbedtls instead of openssl; building openssl on ARM and using 64 bit OS still seems problematic

--- build-kvs-webrtc.sh
+++ build-kvs-webrtc.sh
@@ -6,6 +6,7 @@ sudo apt-get install -y \
   cmake \
   zip \
   libssl-dev \
+  libmbedtls-dev \
   libcurl4-openssl-dev \
   liblog4cplus-dev \
   libgstreamer1.0-dev \
@@ -16,16 +17,13 @@ sudo apt-get install -y \
   gstreamer1.0-plugins-ugly \
   gstreamer1.0-tools
 
-# echo "cloning the amazon-kinesis-video-streams-webrtc-sdk-c repository"
-# git clone --recursive https://github.com/dave-malone/amazon-kinesis-video-streams-webrtc-sdk-c
-
 mkdir -p amazon-kinesis-video-streams-webrtc-sdk-c/build
 cd amazon-kinesis-video-streams-webrtc-sdk-c/build
 
-cmake .. -DBUILD_TEST=TRUE -DBUILD_DEPENDENCIES=FALSE
+cmake .. -DUSE_MBEDTLS=ON -DUSE_OPENSSL=OFF
 make
 
-sudo mkdir -p /opt/
-sudo cp -r amazon-kinesis-video-streams-webrtc-sdk-c/ /opt/
+sudo mkdir -p /opt/amazon-kinesis-video-streams-webrtc-sdk-c
+sudo cp -r amazon-kinesis-video-streams-webrtc-sdk-c/* /opt/amazon-kinesis-video-streams-webrtc-sdk-c/
 
 cd ../..
